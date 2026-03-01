c2f005d1403da01c60b8131a0f60b81cade19923
Nimrod Gaudin <nimrod.gaudin@wheelright.co.uk>
2026-03-01 21:30:48 +0000

    Adding LogLineFormat & ISectionHandler & SectionHandler& DataTimeSectionHandler

---
+++ LogParser/LogLineFormat.cs
@@ -0,0 +1,34 @@
+﻿using LogParser.SectionHandlers;
+
+namespace LogParser;
+
+public class LogLineFormat
+{
+    private List<ISectionHandler> SectionHandlers = [];
+
+    public LogLineFormat(List<SectionHandler> sectionHandlers)
+    {
+        foreach (SectionHandler sectionHandler in sectionHandlers)
+        {
+            ISectionHandler handler = sectionHandler switch
+            {
+                SectionHandler.DateTime => new DateTimeSectionHandler(),
+              //  SectionHandler.LogLevel => new LogLevelSectionHandler(),
+              //  SectionHandler.LogMessage => new LogMessageSectionHandler(),
+              //  SectionHandler.Component => new ComponentSectionHandler(),
+                _ => throw new ArgumentException($"Invalid section handler type: {sectionHandler}")
+            };
+            SectionHandlers.Add(handler);
+        }
+    }
+
+    public virtual int GetNumberOfSections()
+    {
+        return SectionHandlers.Count();
+    }
+
+    public virtual List<ISectionHandler> GetSectionHandlers()
+    {
+        return SectionHandlers;
+    }
+}
--- LogParser/LogLineParser.cs
+++ LogParser/LogLineParser.cs
@@ -1,7 +1,37 @@
-﻿namespace LogParser
+﻿using LogParser.SectionHandlers;
+
+namespace LogParser
 {
     public class LogLineParser
     {
+        public static LogLineParserResult BuildLogLine(string logLine, LogLineFormat logLineFormat)
+        {
+            if (string.IsNullOrEmpty(logLine) || logLineFormat is null || logLineFormat.GetNumberOfSections() == 0) 
+                return new LogLineParserResult(false, [], "LogLine nad LogLineFormat are null or empty");
 
+            try
+            {
+                return BreakLogLineintoSections(logLine, logLineFormat);
+            }
+            catch (Exception ex)
+            {
+                return new LogLineParserResult(true, [], ex.Message);
+            }
+        }
+
+        private static LogLineParserResult BreakLogLineintoSections(string logLine, LogLineFormat logLineFormat)
+        {
+            int numberOfSections = logLineFormat.GetNumberOfSections();
+            List<string> logLineSections = new List<string>();
+            int sectionIndex = 0;   
+            foreach (ISectionHandler sectionHandler in logLineFormat.GetSectionHandlers())
+            {
+                LogLineParserResult sectionResult = sectionHandler.HandleSection(logLine, sectionIndex);
+                if(!sectionResult.Success) return sectionResult;
+                logLineSections.Add(sectionResult.LogLineSections[0]);
+            }
+
+            return new LogLineParserResult(true, logLineSections.ToArray(), string.Empty);
+        }
     }
 }
